84b2387214c17c46690d34549b5d69a9a8b3606f
Pol <polpica@hotmail.com>
2019-01-25 15:31:17 +0100

    "Method is buyable implemented".

--- src/Models/Merchant.java
+++ src/Models/Merchant.java
@@ -1,6 +1,9 @@
 package Models;
 
+import java.util.List;
+
 public class Merchant extends NPC {
+    private List<ProductToSell> products;
 
     public Merchant(String name) {
         super(name);
@@ -9,5 +12,12 @@ public class Merchant extends NPC {
     @Override
     public String appear() {
         return null;
+    }
+
+    public boolean isBuyable(ProductToSell product) {
+        for (ProductToSell n: products) {
+            if (n.equals(product)) return true;
+        }
+        return false;
     }
 }
